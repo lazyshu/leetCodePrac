0e65d25a3ab5b0dcc5c8f5dcf2135dfc71a2cd2a
seuem <lazyslug@kakao.com>
2024-09-28 01:49:21 +0900

    listNode so confusing :(

--- src/September/twentyeighth/PalindromeNumber.java
+++ src/September/twentyeighth/PalindromeNumber.java
@@ -4,22 +4,34 @@ public class PalindromeNumber {
     //https://leetcode.com/problems/palindrome-number/
     public static void main(String[] args) {
         int b = 121;
-        int x = b;
-        if(x<0){
-            System.out.println(false);
-
+        int bb=b;
+        int t,s=0;
+        while (bb != 0) {
+            t = bb % 10; // t= 1
+            s=s*10+t; //1
+            bb /= 10;
         }
-        int reversedX = 0;
-        while (x > 9) {
-            reversedX =reversedX*10+x % 10;
-            x /= 10;
-            System.out.println("x = " + x);
-            System.out.println("reversedX = " + reversedX);
-
-        }
-        reversedX =reversedX*10+x % 10;
-        if (reversedX == b) {
+        if (s == b) {
             System.out.println("true = " + true);
         }
+
+        //361ms
+//        int x = b;
+//        if(x<0){
+//            System.out.println(false);
+//
+//        }
+//        int reversedX = 0;
+//        while (x > 9) {
+//            reversedX =reversedX*10+x % 10;
+//            x /= 10;
+//            System.out.println("x = " + x);
+//            System.out.println("reversedX = " + reversedX);
+//
+//        }
+//        reversedX =reversedX*10+x % 10;
+//        if (reversedX == b) {
+//            System.out.println("true = " + true);
+//        }
     }
 }
